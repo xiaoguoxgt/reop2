2f89324db9bbc859643699b31d28f1e63cbb3c66
xiao <1440438672@qq.com>
2019-07-12 21:05:24 +0800

    添加工程

---
+++ _0001/src/main/java/com/itcast/demo.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+        System.out.println("gitgitgit");
+    }
+}
